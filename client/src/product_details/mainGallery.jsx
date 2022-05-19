c685e9679355757051622e3cc356206cd255c48a
Kevin Niu <kevinkniu@gmail.com>
2022-05-19 09:21:30 -0700

    Added some basic instructions and changed prod_id to be a real product

---
+++ client/src/product_details/mainGallery.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import axios from 'axios';
+
+//may need to import more stuff to begin work
--- client/src/product_page.jsx
+++ client/src/product_page.jsx
@@ -7,12 +7,13 @@ export const ProdPageContext = createContext();
 
 const ProductPage = () => {
   const { cart, setCart } = useContext(AppContext);
-  const [prod_id, setProd] = useState(1);
+  const [prod_id, setProd] = useState(40344);
 
   return (
     <div>
       <ProdPageContext.Provider value={{ cart, setCart, prod_id, setProd }}>
         <h1>This is a Product Page</h1>
+        {/* Add components you want to render here */}
       </ProdPageContext.Provider>
     </div>
   )
