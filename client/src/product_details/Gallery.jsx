f93b39127fcf3ee9b5ad05952c28d5685f02e2e2
Kevin Niu <kevinkniu@gmail.com>
2022-05-19 11:55:55 -0700

    config and testing setup, and gallery WIP

---
+++ client/src/product_details/Gallery.jsx
@@ -0,0 +1,34 @@
+import React, { useState, useContext } from 'react';
+import axios from 'axios';
+import { ProdPageContext } from '../product_page.jsx';
+import { ProdDetailsContext } from './ProductDetails.jsx';
+
+import config from '../../../config.js';
+
+
+//may need to import more stuff to begin work
+
+const Gallery = () => {
+  const {prod_id} = useContext(ProdPageContext);
+  let mainImage = '';
+
+  let getImage = () => {
+    axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/products/${prod_id}/styles`, {
+      headers: {
+        Authorization: config.TOKEN
+      }
+    })
+    .then((results) => {console.log(results)})
+    .catch((err) => console.log(err));
+  }
+
+
+  return (
+    <div>
+      <p onClick={getImage}>This is an image gallery test! Item id: {prod_id}</p>
+    </div>
+  )
+
+}
+
+export default Gallery;
---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  PORT: 3000,
+  TOKEN: "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"
+}
+
+module.exports = config;
