46024635d584d6886bfb37f72eeec128e247550a
Hansol Ji <ji1hansol@gmail.com>
2022-05-24 19:43:55 -0700

    added additional CSS to CarouselContainer in RelatedCarouselList.jsx left: 50% && transform: translateX(-50%) to center carousel list

--- client/src/related_items/RelatedCarouselList.jsx
+++ client/src/related_items/RelatedCarouselList.jsx
@@ -5,8 +5,8 @@ import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 const Carousel = (productDetails) => {
   const [currentImageIdx, setCurrentImageIdx] = useState(0);
-  const display = productDetails.slice(currentImageIdx, (currentImageIdx + 4)); // change to 4
-  const maxDisplay = productDetails.length - 4; // change to 4
+  const display = productDetails.slice(currentImageIdx, (currentImageIdx + 2)); // change to 4
+  const maxDisplay = productDetails.length - 2; // change to 4
   const placeholder = 'http://placecorgi.com/260/180';
 
   const nextSlide = () => {
@@ -52,6 +52,8 @@ const CarouselContainer = styled.div`
   align-items: center;
   position: relative;
   max-width: 1000px;
+  left: 50%;
+  transform: translateX(-50%);
 `;
 
 const CarouselWrapper = styled.div`
