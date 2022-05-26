b56ac5290a2879de59bf6757f57c1f1738d3b2b4
Jongmyung Jeong <yojefff@gmail.com>
2022-05-26 15:20:44 -0700

    Got rid of warnings and console logs and added axios call for post request at AddQuestionForm

--- client/src/q_and_a/AddQuestionForm.jsx
+++ client/src/q_and_a/AddQuestionForm.jsx
@@ -1,18 +1,19 @@
 import React, { useState, useContext, useEffect} from 'react';
+import config from '../../dist/config.js';
+import axios from 'axios';
 import styled from 'styled-components';
 
 const FormSection = styled.div`
   margin: 20px 0px;
   `;
 
-
 const InputText = styled.input`
   margin-right: 10px;
 `;
 
-const AddQuestionForm = ({prod_name }) => {
+const AddQuestionForm = ({prodId, prodName}) => {
 
-    const [question, setQuestion] = useState("");
+  const [question, setQuestion] = useState("");
   const [nickname, setNickname] = useState("");
   const [email, setEmail] = useState("");
 
@@ -20,23 +21,40 @@ const AddQuestionForm = ({prod_name }) => {
   const handleChangeNickname = (e) => {setNickname(e.target.value)};
   const handleChangeEmail = (e) => {setEmail(e.target.value)};
 
-  const handleClickSubmit = () => {};
+  const handleClickSubmit = () => {
+    const url = `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/qa/questions`
+    const data = {
+      body: question,
+      name: nickname,
+      email: email,
+      product_id: prodId
+    }
+    axios.post(url, data, {
+      headers: {
+        Authorization: config.TOKEN
+      }
+    })
+    .then((response) => {
+      console.log('success')
+    })
+    .catch((err) => console.log(err));
+  };
 
   return (
     <div>
       <FormSection>
-        <h4>About the {prod_name}</h4>
+        <h4>About the {prodName}</h4>
       </FormSection>
         <form>
         <FormSection>
           <label> <h5>Your Question *</h5>
-            <textarea name="question" rows="6" cols="60" maxLength="600" required value={question}
+            <textarea name="question" rows="6" cols="60" maxLength="600"  value={question} required ="required"
               onChange={handleChangeQuestion} />
           </label>
         </FormSection>
         <FormSection>
           <label> <h5>What is your nickname? *</h5>
-            <input name="nickname" type="text" placeholder="Example: jackson11!" required size='30' value={nickname}
+            <input name="nickname" type="text" placeholder="Example: jackson11!"  size='30' value={nickname} required="required"
               onChange={handleChangeNickname} />
               <br></br>
               <h5>For privacy reasons, do not use your full name or email address</h5>
@@ -44,7 +62,7 @@ const AddQuestionForm = ({prod_name }) => {
         </FormSection>
         <FormSection>
           <label> <h5>Your email *</h5>
-            <input name="password" placeholder="Why did you like the product or not?" type="text" required size='30' value={email}
+            <input name="password" placeholder="Example: jack@email.com" type="text"  size='30' value={email} required="required"
               onChange={handleChangeEmail} />
               <h5>For authentication reasons, you will not be emailed</h5>
           </label>
