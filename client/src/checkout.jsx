2f657d58dcbd0b6c9c65f127cfcd7290408df930
Kevin Niu <kevinkniu@gmail.com>
2022-05-27 14:35:51 -0700

    initiating checkout workflow

--- client/src/checkout.jsx
+++ client/src/checkout.jsx
@@ -1,15 +1,27 @@
 import React, { useState, createContext, useContext } from "react";
 import { render } from "react-dom";
+import styled from 'styled-components';
 import axios from 'axios';
 import { AppContext } from './index.jsx';
 
 const Checkout = () => {
-  const { cart, setCart } = useContext(AppContext);
+  const { cart, setCart, showModal, setShowModal, modalBodyContent, setModalBodyContent, modalHeaderContent, setModalHeaderContent } = useContext(AppContext);
+
+  const MainWrapper = styled.div`
+  width: 100%;
+  border: 0.5rem solid black;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
   return (
-    <div>
+    <>
       <h1>This is a Checkout Page</h1>
-    </div>
+      <MainWrapper>
+        <h1>Cart Here</h1>
+      </MainWrapper>
+    </>
   )
 }
 
