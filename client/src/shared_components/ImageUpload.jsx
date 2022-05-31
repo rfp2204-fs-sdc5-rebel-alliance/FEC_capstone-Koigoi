fc34f7b0877e2d5e06de0e427c90e7605d55a0a0
Neil <neiljohnson92@gmail.com>
2022-05-31 11:59:43 -0700

    removed importing context

--- client/src/shared_components/ImageUpload.jsx
+++ client/src/shared_components/ImageUpload.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import config from '../../dist/config.js';
 import styled from 'styled-components';
